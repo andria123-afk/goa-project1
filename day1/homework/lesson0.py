66db8879e4fadfad2d83b5930fad05133dcb9afc
andria123-afk <janelidzeandria06@gmail.com>
2024-09-16 19:31:20 +0400

    first commit

---
+++ day1/homework/lesson0.py
@@ -0,0 +1,33 @@
+name = 'andria'
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# 'andria' არის ცვლადისთვის მნიშვნელობა
+
+surname = 'janelidze'
+
+print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტში
+
+name = 'andria' #ეს str (string) ტიპის ცვლადი
+age = 12 #ეს არის int (integer) მთელი რიცხვი
+height = 146.5 #ეს არის fllot ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programing = True 
+is_ugly = False  #snakecase ( უბრალოდ წერის სტილი სტანდარტული)
+
+Ugly = False # ჯავასკრიპტული cameLcase
+
+
+print(name +''+ surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+print(name + age)
+
+# print(type(age))
+# print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programing))
+
+print(name +' '+ str(age))
---
+++ day3/homework/lesson1.py
@@ -0,0 +1,42 @@
+number1 = "num1" #ესენი num1  და  number1  უდრიან ერთმანეთს
+number2 = "num2" #ესენი num2  და  number2  უდრიან ერთმანეთს
+
+num1 = 5   #num1 უდრის 5
+num2 = 10  #num2 უდრის 10
+print(num1 + num2)  #ჩვენ გამოვიყენეთ დაბეჭვდის ტექნიკა num1 = 5   num2 = 10   num1 + num2 = 15   5 + 10 = 15
+
+
+
+
+
+age = 13 
+age_in_5_years = 18
+print(age_in_5_years)
+
+
+
+
+age_me = 13 
+age_parent = 39 
+print(age_me * age_parent)
+
+
+age_me_2 = 13 
+age_sister =16
+age_perent_2 =38
+age_parent = 39 
+
+age_me_in_2_years = 15
+age_sister_in_2_years = 18
+age_father = 40
+age_mother = 41
+
+print(age_me_in_2_years)
+print(age_sister_in_2_years)
+print(age_father)
+print(age_mother)
+
+
+name = "andria"
+name = "noe"
+print(name)
