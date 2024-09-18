a16a958ef5f97c3ed6df640d0b6c1c00d90495ba
andria123-afk <janelidzeandria06@gmail.com>
2024-09-18 19:31:42 +0400

    4 commit

---
+++ day4/homework/lesson4.py
@@ -0,0 +1,32 @@
+# bug არის  error  და debugging არის error-ის გამოსწორება
+#1name = aleko
+#2name = andria
+#1food = xinkali
+
+# გამოსწორება 
+
+#name = "andria"
+#name = "aleko"
+#food = "xinkali"
+
+#მათემატიკა
+
+num1 = 1000
+num2 = 55000
+print(num1 + num2)
+print(num1 * num2)
+print(num2 - num1)
+print(num2 / num1)
+
+granny_age = 60
+grandpa_age = 64
+my_age = 13 
+my_sis = 16
+mother_age = 40
+fathers_age = 39
+print(granny_age)
+print(grandpa_age )
+print(my_age)
+print(my_sis)
+print(mother_age)
+print(fathers_age)
