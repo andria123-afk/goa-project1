03ed393354617fcc30af4ce460d4326943cbd70e
andria123-afk <janelidzeandria06@gmail.com]>
2024-09-30 13:33:44 +0400

    first commit

---
+++ Day7/homework/lesson.py
@@ -0,0 +1,18 @@
+my_mom = 40
+my_dad = 39 
+
+my_mom_25_yr = 65
+my_dad_25_yr = 64
+
+print(my_mom_25_yr)
+print(my_dad_25_yr)
+
+name_someone = "nargiza"
+print(type(name_someone))
+
+nargiza_age = 65 
+print(str(nargiza_age))
+
+#str = გამოიყენება ნებისმიერ სიტყვათან ასევე რიცხვებთან და მთავარი არის ბრჭყალები რითაც ვგებულობთ რომ სტრინგია მაგ: name = 'nargiza' name = "natela" name = "natela' ბოლო მაგალითი არ შეიძლება ბრჭყალები ერთიდა იგივე უნდა იყოს
+#int = გამოიყენება მხოლოდ რიცხვებზე დამატებითი არაფერი არ სჭირდება მაგ: nargiza_age = 65
+#float = ნებისმიერი წილადი რიცხვი მაგ: nargiza_height = 1.68
