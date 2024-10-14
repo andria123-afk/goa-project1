e3221f90058c597ef95379856ff0f30d3019d067
andria123-afk <janelidzeandria06@gmail.com>
2024-10-14 15:34:55 +0400

    11 commit

---
+++ day9/classwork/main.py
@@ -0,0 +1,4 @@
+age = 17
+adult = 18
+#17 is underaged
+
---
+++ day9/homework/lesson.py
@@ -0,0 +1,19 @@
+age_someone = 29
+print(29//10)
+
+boy = "vano"
+me = "andria"
+
+
+
+num1 = 20 
+num2 = 10
+print(20/10)
+
+age = 20
+print(20%2)
+
+# მეორე მაგალითი მაინც 
+age = 19
+print(19%2)
+
