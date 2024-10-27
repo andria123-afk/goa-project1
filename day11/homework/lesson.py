3e7099131e121f13a0050b44e1b98be3a5cd9aa5
andria123-afk <janelidzeandria06@gmail.com>
2024-10-27 22:15:05 +0400

    13

---
+++ day11/homework/lesson.py
@@ -0,0 +1,6 @@
+print(True and False) 
+print(True and True)
+print(True or False)
+print(True or True)
+
+      
---
+++ day13/homework/lesson.py
@@ -0,0 +1,46 @@
+i = 1
+
+for i in range(1,11):
+    print(i)
+
+i = 1
+
+for i in range(50,150):
+    print(i)
+
+
+for i in range(200,500,2):
+    print(i)
+
+for i in range(1,50,2):
+    print(i)
+
+surname = "janelidze"
+
+for i in surname:
+    print(i)
+
+
+i = 1
+
+while i < 15:
+    print(i)
+    i += 1
+
+i = 50 
+while i > 0:
+    print(i)
+    i -= 1
+
+i = 10 
+while i < 70:
+    print(i)
+    i += 2
+
+
+i = 25
+while i < 55:
+    print(i)
+    i += 2
+
+
