3c13d036cb7e142a8afb2708fc316559fadfe024
andria123-afk <janelidzeandria06@gmail.com>
2024-11-03 21:31:39 +0400

    20

---
+++ day17/classwork/classwork.py
@@ -0,0 +1,4 @@
+names = ["andria", "giorgi", "keso", "data", "noe", "guji", "temo"]
+
+for i in names:
+ for print("i"[2])
---
+++ day17/homework/lesson.py
@@ -0,0 +1,24 @@
+fav_anime = ["naruto", "jujutsukaisen", "dragonball", "demonslayers", "one peace"] 
+print(fav_anime[0])
+print(fav_anime[1])
+print(fav_anime[2])
+print(fav_anime[3])
+print(fav_anime[4])
+
+
+names  = [13, "andria", "tamuna", "gio", "vano", "wener", "naruto", "saturo gojo", "hatake kakashi", "i am the GHOST of the uchiha"]
+for i in names:
+    print (i)
+
+numbers = 12
+
+if numbers > 10:
+    print("more then 10")
+
+elif numbers == 10:
+    print("equals to 12")
+
+else:
+    print ("less the 10")
+
+
